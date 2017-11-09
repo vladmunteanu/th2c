e47cd36916a5d6a9224135489bca256a60bb2643
Vlad Stefan Munteanu <vlad.munteanu@avira.com>
2017-11-09 17:54:49 +0200

    Added basic tests for client and connection, wip

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+"""
+Things worth testing:
+
+Client:
+- add request,
+- setting max_active_streams, making sure it actually controls the
+maximum number of opened streams
+
+-
+
+"""
---
+++ test/test_connection.py
@@ -0,0 +1,52 @@
+import mock
+
+from tornado import gen
+from tornado.testing import AsyncTestCase, gen_test
+from tornado.httpclient import HTTPRequest
+
+from th2c.client import AsyncHTTP2Client
+from th2c.connection import HTTP2ClientConnection
+from th2c.stream import HTTP2ClientStream
+from th2c.exceptions import RequestTimeout
+
+
+class HTTP2ClientConnectionTestCase(AsyncTestCase):
+
+    @gen_test
+    def test_connect_timeout(self):
+        tcp_client = mock.MagicMock()
+        connection_closed = mock.MagicMock()
+
+        connection = HTTP2ClientConnection(
+            "host", 1234, tcp_client, False, self.io_loop,
+            connect_timeout=1,
+            on_connection_closed=connection_closed
+        )
+
+        connection.connect()
+
+        yield gen.sleep(1.1)
+
+        connection_closed.assert_called_once()
+        self.assertEqual(connection.timed_out, True)
+
+    def test_connection_cleanup(self):
+        tcp_client = mock.MagicMock()
+        connection_closed = mock.MagicMock()
+
+        connection = HTTP2ClientConnection(
+            "host", 1234, tcp_client, False, self.io_loop,
+            on_connection_closed=connection_closed
+        )
+
+        connection.connect()
+
+        self.assertEqual(connection.closed, False)
+
+        exc = Exception("some error")
+        connection.close(exc)
+
+        connection_closed.assert_called_once_with(exc)
+        self.assertEqual(connection.is_ready, False)
+        self.assertEqual(connection.is_connected, False)
+        self.assertEqual(connection.closed, True)
