11498f579126d5a9770f27ec90fb1db8f6a47327
Vlad Stefan Munteanu <vlad.munteanu@avira.com>
2017-11-14 21:49:20 +0200

    Print message before listening

--- test_server/main.go
+++ test_server/main.go
@@ -12,9 +12,9 @@ func main() {
 
 	http.HandleFunc("/", index_main)
 
-	srv.ListenAndServeTLS("test_server/certs/localhost.cert", "test_server/certs/localhost.key")
+	fmt.Println("Listening for HTTP/2 connections on port 8080")
 
-	fmt.Println("Listening for HTTP/2 connections on localhost:8080")
+	srv.ListenAndServeTLS("test_server/certs/localhost.cert", "test_server/certs/localhost.key")
 }
 
 func index_main(w http.ResponseWriter, r *http.Request) {
