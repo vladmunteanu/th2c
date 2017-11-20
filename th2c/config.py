ac51a70f881b7f95be4ad5a05d74d370e3f9a3ad
Vlad Stefan Munteanu <vlad.munteanu@avira.com>
2017-11-20 16:19:57 +0200

    Added comments for settings, set default reconnect to 1 second

--- th2c/config.py
+++ th2c/config.py
@@ -1,7 +1,18 @@
+# Default size of the flow control window.
+# Used to initialize the connection flow control window,
+# as well as the window sizes for initiated streams.
 DEFAULT_WINDOW_SIZE = 65535  # bytes
+
+# Maximum size of an HTTP/2 frame.
 MAX_FRAME_SIZE = 16384  # bytes
+
+# Maximum number of concurrent streams (concurrent requests).
 MAX_CONCURRENT_STREAMS = 100
 
+# Default number of seconds to wait for the connection to be established.
 DEFAULT_CONNECTION_TIMEOUT = 5  # seconds
 
-DEFAULT_RECONNECT_INTERVAL = 5  # seconds
+# Default number of seconds to wait before trying to reconnect.
+# This setting is used only if AsyncHTTP2Client has been instantiated
+# with auto_reconnect=True.
+DEFAULT_RECONNECT_INTERVAL = 1  # seconds
