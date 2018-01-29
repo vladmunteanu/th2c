ce556f121e7c759e973acfed7a6f16fc158e3650
Vlad Stefan Munteanu <vlad.munteanu@avira.com>
2018-01-29 16:57:51 +0200

    Set Go workspace

--- vm/provision.sh
+++ vm/provision.sh
@@ -6,3 +6,9 @@ apt-get install -y golang-go
 
 cd /opt/dev/th2c
 pip install -r requirements.txt
+
+# set GOPATH
+echo "export GOPATH=/opt/dev/goworkspace/" > /etc/profile.d/gopath.sh
+
+# get the http2 package
+go get golang.org/x/net/http2
